92975ef17bc05427b15fbfa0d650b84b72c431dc
kairi <kairi.satellite@gmail.com>
2026-04-08 11:31:00 +0900

    Refactor build script to simplify archive creation

--- scripts/build.js
+++ scripts/build.js
@@ -6,16 +6,11 @@ const path = require('node:path');
 const archiver = require('archiver');
 const ROOT = path.join(__dirname, '..');
 
-
 const build = async () => {;
   const zipName = 'unistyle.zip';
   fs.mkdirSync(path.join(ROOT, 'dist'), { recursive: true });
   const output = fs.createWriteStream(path.join(ROOT, 'dist', zipName));
-
-
-  const archive = archiver('zip', {
-    zlib: { level: 9 },
-  });
+  const archive = archiver('zip', { zlib: { level: 9 } });
   archive.pipe(output);
   archive.glob('LICENSE');
   process.chdir(path.join(ROOT, 'src'));
